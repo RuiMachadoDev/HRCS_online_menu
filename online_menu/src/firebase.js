262320eb66aa529e6aebdff5b28abd42e5a94475
Rui Machado <ruifilipemachado@hotmail.com>
2024-08-16 12:05:09 +0100

    Create and connect Firebase with the app.

---
+++ online_menu/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLk_wcH40OKlV_5kPKrvDal5m8nBlX6Fg",
+  authDomain: "hrcsonlinemenu.firebaseapp.com",
+  projectId: "hrcsonlinemenu",
+  storageBucket: "hrcsonlinemenu.appspot.com",
+  messagingSenderId: "148845543687",
+  appId: "1:148845543687:web:e0618fdd56906f177d45a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
